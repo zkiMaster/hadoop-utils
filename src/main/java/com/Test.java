03b68043bb645097f6307b4e1868fadc62d3be8d
zhoukui <zhoukui@datastory.com.cn>
2019-06-26 14:26:25 +0800

    test ssh keygen

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("test-1:"+1);
         System.out.println("test-1:"+2);
         System.out.println("test-2:"+1);
+        System.out.println("aaa"+1);
     }
 }
